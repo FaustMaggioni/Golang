72b304746d413e810fdcf40bd7a7d25c8d2fa999
Faustino <Faustino@LAPTOP-VI7AL9FL>
2021-04-16 17:02:28 -0300

    problema al importar structs

---
+++ API_todo/go.mod
@@ -0,0 +1,3 @@
+module _/C_/Users/Faustino/Documents/GetGOing/API_todo/structs
+
+go 1.16
---
+++ API_todo/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    //"fmt"
+	"net/http"
+	"encoding/json"
+	"./structs"
+    )
+
+// JSON API
+
+
+func main(){
+	const ping="/ping"
+	mux := http.NewServeMux()
+	mux.HandleFunc(ping,func(w http.ResponseWriter, r *http.Request){
+		if r.Method == http.MethodGet {
+			data:= structs.Response{
+				Code: http.StatusOK,
+				Body: "pong",
+			}
+			
+			JSON.NewEncoder(w).Encode(data)
+		}
+	})
+	http.ListenAndServe(":3000", mux)
+}
